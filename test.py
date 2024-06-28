8e91b96d26c5aaf27be0e6f11692b280069b281d
jag-prabhakaran <73809351+jag-prabhakaran@users.noreply.github.com>
2024-06-27 22:19:26 -0700

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import json
-
-score = [0, 1, 2]
-
-with open("file.json", 'w') as f:
-    # indent=2 is not needed but makes the file human-readable 
-    # if the data is nested
-    json.dump(score, f, indent=2) 
